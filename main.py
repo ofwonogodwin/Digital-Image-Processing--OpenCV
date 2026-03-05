f1f41c0c422f26277af56cfa3dc840962b6ec66b
Godwin Ofwono <godwinofwono933@gmail.com>
2026-03-05 16:46:17 +0300

    Set development structure to Use

---
+++ main.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+def task1():
+    pass
+
+def task2():
+    pass
+
+def task3():
+    pass
+
+# continue like that...
+
+if __name__ == "__main__":
+    task1()
+    task2()
