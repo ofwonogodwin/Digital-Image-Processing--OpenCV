998227fc163659b936f5379660fb1c045cdf7d31
Godwin Ofwono <godwinofwono933@gmail.com>
2026-03-05 17:20:13 +0300

    Finished Task 1 of loading the Image and doing some analysis on it

--- main.py
+++ main.py
@@ -2,16 +2,31 @@ import cv2
 import numpy as np
 
 def task1():
-    pass
+    try:
+        image = cv2.imread("Man.jpeg")
 
-def task2():
-    pass
+        if image is None:
+            raise FileNotFoundError("Image not found!")
 
-def task3():
-    pass
+        height, width, channels = image.shape
+        print("Dimensions:", image.shape)
 
-# continue like that...
+        print("Data Type:", image.dtype)
+
+        total_pixels = height * width
+        print("Total Pixels:", total_pixels)
+
+        print("Min Pixel Value:", image.min())
+        print("Max Pixel Value:", image.max())
+
+        center_pixel = image[height//2, width//2]
+        print("Center Pixel (BGR):", center_pixel)
+
+    except Exception as e:
+        print("Error:", e)
+
 
 if __name__ == "__main__":
     task1()
-    task2()
+    #task2()
+    #task3()
